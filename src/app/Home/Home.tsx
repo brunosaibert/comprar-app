be4a0d78b79308a53009e9b6b3971f958642af7c
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-07-25 17:52:18 -0300

    feat: create button component

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,10 +1,14 @@
 import { View, Image } from "react-native";
+
+import { Button } from "@/components/Button";
+
 import { styles } from "./Home.styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <Button title="Entrar" />
     </View>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, Pressable, PressableProps } from "react-native";
+
+import { styles } from "./Button.styles";
+
+type ButtonProps = PressableProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <Pressable style={styles.container} {...rest}>
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+}
