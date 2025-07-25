f7fd9af5f0b0ff73ca8cfbc2229dbefb5696e147
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-07-25 18:31:40 -0300

    feat: add filter status icon

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#2c4681",
+    backgroundColor: "#2c46b1",
     height: 48,
     width: "100%",
     borderRadius: 8,
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -3,6 +3,8 @@ import { Text, Pressable, type PressableProps } from "react-native";
 
 import { FilterStatus } from "@/types/FilterStatus.types";
 
+import { StatusIcon } from "../StatusIcon";
+
 import { styles } from "./Filter.styles";
 
 type FilterProps = PressableProps & {
@@ -16,6 +18,7 @@ export function Filter({ status, isActive, ...rest }: FilterProps) {
       style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
       {...rest}
     >
+      <StatusIcon status={status} />
       <Text style={styles.title}>
         {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
       </Text>
