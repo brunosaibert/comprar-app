25a8aa59155f397f8f3a7ebdf325cf14b5902ad8
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-07-25 18:04:53 -0300

    feat: adjustment of the home structure

--- src/app/Home/Home.styles.ts
+++ src/app/Home/Home.styles.ts
@@ -5,12 +5,25 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     backgroundColor: "#d0d2d8",
-    paddingTop: 62,
-    padding: 24,
-    gap: 24
+    paddingTop: 62
   },
   logo: {
     height: 34,
     width: 134
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 8,
+    marginTop: 42
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#fff",
+    width: "100%",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24
   }
 });
